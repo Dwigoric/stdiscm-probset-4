ad430d46c501644bd861f001ef860ef4493f8621
Joshua Permito <git.commits@dwigoric.dev>
2025-04-07 10:48:42 +0800

    chore(courselist): set up courselist backend node

---
+++ node_courselist/main.ts
@@ -0,0 +1,12 @@
+// @ts-types="npm:@types/express@4.17.15"
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.send("Welcome to the Dinosaur API!");
+});
+
+app.listen(8041);
+console.log(`Server is running on http://localhost:8041`);
